f1d1fcf8247881203186c23144f9afb340626deb
zalik-agency <dariusz.zalik.agency@gmail.com>
2025-12-02 02:25:24 +0100

    ­ƒÅå Initial commit: Legendary Zalik.Agency Website

---
+++ git-init.bat
@@ -0,0 +1,30 @@
+# Git initialisieren
+git init
+
+# HTML-Datei hinzufügen (umbenennen zu index.html)
+cp zalik-agency.html index.html
+
+# Commit erstellen
+git add .
+git commit -m "🏆 Initial commit: Legendary Zalik.Agency Website"
+
+# Remote hinzufügen und pushen
+git remote add origin https://github.com/zalik-agency/zalik-agency-legendary.git
+git branch -M main
+git push -u origin main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
